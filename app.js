4f0f85bf4ef1b068dfa3498245539dbee4cd2e11
Iriagamb <77029483+Iriagamb@users.noreply.github.com>
2024-06-27 12:01:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let randomNumber = Math.random() * 10;
